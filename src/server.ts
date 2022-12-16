b7102e2721988e3f078bad3dd107a47db7c7b4b0
nixer89 <pbnixer@gmail.com>
2022-12-16 13:27:30 +0100

    stringify

--- src/server.ts
+++ src/server.ts
@@ -244,7 +244,7 @@ const start = async () => {
           }
   
           if(!isBlocked) {
-            console.log("RATE LIMIT HIT BY: " + ip + " from " + req.hostname + " to: " + req.routerPath + " with: " + req.params);
+            console.log("RATE LIMIT HIT BY: " + ip + " from " + req.hostname + " to: " + req.routerPath + " with: " + JSON.stringify(req.params));
             error.message = 'You are sending too many requests in a short period of time. Please calm down and try again later or contact us to request elevated limits: @XrplServices (on twitter)'
           } else {
             showHeaders++;
